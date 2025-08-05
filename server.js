f6f1e37cb8c54369b18137a758e8b026ea880647
hamed435 <eng.mahmoud220@gmail.com>
2025-08-06 02:32:30 +0300

    create docker file

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use("/api/about", aboutRoutes);
 app.use("/api/settings", settingRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
