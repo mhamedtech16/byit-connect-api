eb9fc861ea601261831bc4f502f76bfe525ea4ab
hamed435 <eng.mahmoud220@gmail.com>
2025-08-05 16:54:42 +0300

    first commit

--- controllers/aboutController.js
+++ controllers/aboutController.js
@@ -1,56 +1,79 @@
-const About = require('../models/Settings/About');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
-
+const About = require("../models/Settings/About");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.addAbout = async (req, res) => {
-  const { title_en, title_ar, description_en, description_ar,img,
-  facebook,
-  instagram,
-  website,
-  whatsapp,
-  phone } = req.body;
-
-  const about = await About.create({title_en, title_ar, description_en, description_ar,img,
-  facebook,
-  instagram,
-  website,
-  whatsapp,
-  phone });
-  res.status(201).json({success:true,data: about});
-};
-
-
-
-exports.updateAbout = async (req, res) => {
-  const { title_en, title_ar, description_en, description_ar,img,
-  facebook,
-  instagram,
-  website,
-  whatsapp,
-  phone } = req.body;
-
-  const updatedDoc = await About.findOneAndUpdate(
-    {},
-    { $set: { title_en, title_ar, description_en, description_ar,img,
+  const {
+    title_en,
+    title_ar,
+    description_en,
+    description_ar,
+    img,
     facebook,
     instagram,
     website,
     whatsapp,
-    phone } },
+    phone,
+  } = req.body;
+
+  const about = await About.create({
+    title_en,
+    title_ar,
+    description_en,
+    description_ar,
+    img,
+    facebook,
+    instagram,
+    website,
+    whatsapp,
+    phone,
+  });
+  res.status(201).json({ success: true, data: about });
+};
+
+///////// Update About
+exports.updateAbout = async (req, res) => {
+  const {
+    title_en,
+    title_ar,
+    description_en,
+    description_ar,
+    img,
+    facebook,
+    instagram,
+    website,
+    whatsapp,
+    phone,
+  } = req.body;
+
+  const updatedDoc = await About.findOneAndUpdate(
+    {},
+    {
+      $set: {
+        title_en,
+        title_ar,
+        description_en,
+        description_ar,
+        img,
+        facebook,
+        instagram,
+        website,
+        whatsapp,
+        phone,
+      },
+    },
     { new: true, runValidators: true }
-  )
+  );
   // const about = await About.findByIdAndUpdate(,{title_en, title_ar, description_en, description_ar,img,
   // facebook,
   // instagram,
   // website,
   // whatsapp,
   // phone });
-  res.status(201).json({success:true,data: about});
+  res.status(201).json({ success: true, data: about });
 };
 
 exports.getAbout = async (req, res) => {
   const about = await About.find();
-  res.json({data: about});
+  res.json({ data: about });
 };
