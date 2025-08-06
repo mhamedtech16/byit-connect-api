b255127c55483608616d281027313b480b44b5f2
mhamedtech16 <mhamed.tech16@gmail.com>
2025-08-06 03:54:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
-const userRoutes = require("./routes/userRoutes");
-const clientRoutes = require("./routes/clientRoutes");
-const countriesRoutes = require("./routes/countriesRoutes");
-const locationsRoutes = require("./routes/locationsRoutes");
-const categoriesRoutes = require("./routes/categoriesRoutes");
-const rolesRoutes = require("./routes/rolesRoutes");
-const permissionsRoutes = require("./routes/permissionsRoutes");
-const socialLinksRoutes = require("./routes/socialLinksRoutes");
-const aboutRoutes = require("./routes/aboutRoutes");
-const settingRoutes = require("./routes/settingRoutes");
-const dashboardRoutes = require("./routes/dashboardRoutes");
+// const userRoutes = require("./routes/userRoutes");
+// const clientRoutes = require("./routes/clientRoutes");
+// const countriesRoutes = require("./routes/countriesRoutes");
+// const locationsRoutes = require("./routes/locationsRoutes");
+// const categoriesRoutes = require("./routes/categoriesRoutes");
+// const rolesRoutes = require("./routes/rolesRoutes");
+// const permissionsRoutes = require("./routes/permissionsRoutes");
+// const socialLinksRoutes = require("./routes/socialLinksRoutes");
+// const aboutRoutes = require("./routes/aboutRoutes");
+// const settingRoutes = require("./routes/settingRoutes");
+// const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -28,17 +28,17 @@ app.use(express.urlencoded({ extended: true }));
 //   .then(() => console.log("✅ Connected to MongoDB"))
 //   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
-app.use("/api/users", userRoutes);
-app.use("/api/clients", clientRoutes);
-app.use("/api/countries", countriesRoutes);
-app.use("/api/locations", locationsRoutes);
-app.use("/api/categories", categoriesRoutes);
-app.use("/api/roles", rolesRoutes);
-app.use("/api/permissions", permissionsRoutes);
-app.use("/api/socialLinks", socialLinksRoutes);
-app.use("/api/about", aboutRoutes);
-app.use("/api/settings", settingRoutes);
-app.use("/api/dashboard", dashboardRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/clients", clientRoutes);
+// app.use("/api/countries", countriesRoutes);
+// app.use("/api/locations", locationsRoutes);
+// app.use("/api/categories", categoriesRoutes);
+// app.use("/api/roles", rolesRoutes);
+// app.use("/api/permissions", permissionsRoutes);
+// app.use("/api/socialLinks", socialLinksRoutes);
+// app.use("/api/about", aboutRoutes);
+// app.use("/api/settings", settingRoutes);
+// app.use("/api/dashboard", dashboardRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
